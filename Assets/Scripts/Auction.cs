567a498cf2e2039e0e598c69a1dd5fb932b11c78
Kalaith <talast@gmail.com>
2018-02-03 16:55:08 +1100

    added 3 screens, also made the buttons navigate between screens.

--- Assets/Scripts/Auction.cs
+++ Assets/Scripts/Auction.cs
@@ -17,4 +17,5 @@ public class Auction : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
---
+++ Assets/Scripts/LoadMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour {
+
+    public void LoadGameMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+    public void LoadGameAuction() {
+        SceneManager.LoadScene("Auction");
+    }
+    public void LoadGamePortfolio() {
+        SceneManager.LoadScene("Portfolio");
+    }
+    public void LoadGameRanking() {
+        SceneManager.LoadScene("Ranking");
+    }
+    public void LoadGameResearch() {
+        SceneManager.LoadScene("Research");
+
+    }
+
+}
