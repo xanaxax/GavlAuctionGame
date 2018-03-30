fadc36cbacb0822b8c3e4609e01fdbe4e0643065
xanaxax <37176308+xanaxax@users.noreply.github.com>
2018-03-30 23:45:29 +1100

    Update Auction.cs

--- Assets/Scripts/Auction.cs
+++ Assets/Scripts/Auction.cs
@@ -8,37 +8,17 @@ using UnityEngine.UI;
 
 public class Auction : MonoBehaviour {
 	public GameObject LogText;
+	public GameObject CurrentPrice;
 	public int bidnumber = 100000;
 	public int bidincrease;
 	public Button BidBtn;
 	public int userbalance = 500000;
-	public int topbid;
+	public int topbid = 100000;
+	public int topbidincrease;
 	//topbid need to be accessed from Firebase server
 
 	public void RandomGenerate()
 	{
-		topbid = Random.Range (100000, 800000);
-		// Get topbid code from firebase
-		//	.GetReference("Leaders")
-		//	.GetValueAsync().ContinueWith(task => {
-		//		if (task.IsFaulted) {
-					// Handle the error...
-		//		}
-		//		else if (task.IsCompleted) {
-		//			DataSnapshot snapshot = task.Result;
-					// Do something with snapshot...
-		//		}
-		//  Listen to firebase events: topbid changes 
-		//	.GetReference("Leaders")
-		//	.ValueChanged += HandleValueChanged;
-		//}
-		//void HandleValueChanged(object sender, ValueChangedEventArgs args) {
-		//	if (args.DatabaseError != null) {
-		//		Debug.LogError(args.DatabaseError.Message);
-		//	return;
-		//	}
-		// Do something with the data in args.Snapshot
-		//
 		if (userbalance < bidnumber) {
 			LogText.GetComponent<Text> ().text += "\n You don't have the balance!";
 		} 
@@ -48,9 +28,12 @@ public class Auction : MonoBehaviour {
 		}
 
 		else {
-			bidincrease = Random.Range (100000, 200000);
+			bidincrease = Random.Range (100000, 500000);
 			bidnumber = bidnumber + bidincrease;
+			topbidincrease = Random.Range (-100000, 100000);
+			topbid = bidnumber + topbidincrease;
 			LogText.GetComponent<Text> ().text += "\n New bid: $" + bidnumber; 
+			CurrentPrice.GetComponent<Text> ().text = "$" + topbid;
 			//return the Firebase server with user's new bid number
 		}
 
