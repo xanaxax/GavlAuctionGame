1c3e2046de8d9cd549efa78e3d0d6e0da6fa5f44
xanaxax <37176308+xanaxax@users.noreply.github.com>
2018-03-30 22:48:17 +1100

    Add files via upload

--- Assets/Scripts/Auction.cs
+++ Assets/Scripts/Auction.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/*
- * A current auction in process, an auction is made up of 2 players, a house an auctioneer
- */
+using UnityEngine.UI;
 
 public class Auction : MonoBehaviour {
+	public GameObject LogText;
+	public int bidnumber = 100000;
+	public int bidincrease;
+	public Button BidBtn;
+	public int userbalance = 500000;
+	public int topbid;
+	//topbid need to be accessed from Firebase server
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void RandomGenerate()
+	{
+		topbid = Random.Range (100000, 500000);
+		if (userbalance < bidnumber) {
+			LogText.GetComponent<Text> ().text += "\n You don't have the balance!";
+		} 
+		else if (topbid < bidnumber) {
+			LogText.GetComponent<Text> ().text += "\n You Win the Bid!";
+			//return Firebase server with bidding end signal
+		}
 
+		else {
+			bidincrease = Random.Range (100000, 200000);
+			bidnumber = bidnumber + bidincrease;
+			LogText.GetComponent<Text> ().text += "\n New bid: $" + bidnumber; 
+			//return the Firebase server with user's new bid number
+		}
+
+	}
+		
 }
